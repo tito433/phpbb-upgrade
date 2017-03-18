c250908a317f563caf7ca52ad666d7938c4747e1
tito <tito@cefalo.com>
2017-03-18 11:06:44 +0600

    tool cleanup

--- cleanup.bat
+++ cleanup.bat
@@ -3,13 +3,13 @@
 SET wamp_dir= C:\wamp
 SET forum_path=%wamp_dir%\www\dreamteam
 
-SET upgrade_path=G:\Personal\dreamteam\
+SET upgrade_path=G:\Personal\dreamteam
 
 
-echo Upgradation assist tool
+echo bddreamteam phpbb upgrade tool.
 
-CHOICE /C 123C /M "Please select file source. 1:=live, 2:=phpbb3.0.14, 3:=phpbb3.2, C:=Cancel"
-IF ERRORLEVEL 4 GOTO End
+CHOICE /C 123C /M "Forum source ?1:=live, 2:=phpbb3.0.14, 3:=phpbb3.2, C:=Cancel"
+IF ERRORLEVEL 4 GOTO StepTwo
 IF ERRORLEVEL 3 GOTO LabelThree
 IF ERRORLEVEL 2 GOTO LabelTwo
 IF ERRORLEVEL 1 GOTO LabelOne
@@ -30,7 +30,25 @@ goto DoCopy
 
 :DoCopy
 echo Copying:%src_file% 
-xcopy /s/y %src_file% %forum_path% /E /Q
+rmdir /s %forum_path%
+mkdir %forum_path%
+xcopy %src_file% %forum_path% /E /Q
+
+:StepTwo
+CHOICE /M "Database restore?"
+IF ERRORLEVEL 2 GOTO End
+IF ERRORLEVEL 1 GOTO RestoreDatabase
+
+GOTO End
+
+:RestoreDatabase
+
+SET forum_db_name=db1164312_dremteam
+SET forum_db_path=G:\Personal\dreamteam\database\%forum_db_name%_clean.sql
+SET PATH=%PATH%;C:\wamp\bin\mysql\mysql5.6.17\bin;
+
+echo Cleaning MySQL for schema %forum_db_name%
+mysql -uroot -p %forum_db_name% < %forum_db_path%
 
 
 :End
