70186ab3da243c1aa172bf706d914fbb25e4a7e1
tito <tito@cefalo.com>
2017-04-07 09:42:19 +0600

    mod

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # phpbb-upgrade
 I'm trying to upgrade a phpbb 3.0.5 to latest phpbb 3.2
+
+
+
+[Restore default phpBB-3.0.5 fiels]
+adm/index.php
+includes\acp\acp_users.php
+language\en\acp\board.php
+
+set_time_limit(3600);
+
+[Database chagnes]
+UPDATE users SET `user_type`=3, `group_id`=5 WHERE `username`='tito';
+
+mysqldbcompare --server1=root@localhost db1164312_dremteam:phpbb32 --run-all-tests --skip-data-check --skip-row-count --skip-checksum-table --difftype=sql| clip
+
+
+[select all new files during upgrade]
+var rad=document.querySelectorAll('input[value="1"]');
+rad.forEach(function(r){r.checked=true;});
+
+[Upgrade task]
+1. Upgrade to 3.0.14 using phpBB-3.0.5_to_3.0.14\install
+2. use save\update_3.0.5_to_3.0.14.zip for replace all files.
+3. Use fresh copy of phpbb3.0.14 files.
+4. copy phpBB-3.0.14_to_3.2.0\install to root
+5. change style_name to style_path in styles table.
+6. add column 'style_parent_id' to styles table.
+7. execute install/update
+
+ALTER TABLE `users` CHANGE `user_password` `user_password` VARCHAR(256) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '';
+#UPDATE `modules` SET `module_basename`=CONCAT(module_class,'_',module_basename) WHERE module_basename like '%main%'
--- cleanup.bat
+++ cleanup.bat
@@ -3,56 +3,87 @@
 SET wamp_dir=C:\wamp
 SET forum_path=%wamp_dir%\www\dreamteam
 SET upgrade_path=G:\Personal\dreamteam
+SET forum_db_name=db1164312_dremteam
+SET PATH=%PATH%;C:\wamp\bin\mysql\mysql5.6.17\bin;
 
-echo bddreamteam phpbb upgrade tool.
+echo %forum_db_name% upgrade tool.
 
 
-CHOICE /C 123C /M "Forum source ?1:=live, 2:=phpbb3.0.14, 3:=phpbb3.2, C:=Cancel"
-IF ERRORLEVEL 4 GOTO StepTwo
+CHOICE /C 1234C /M "phpbb Version ?1:=phpbb3.0.5, 2:=phpbb3.0.12, 3:=phpbb3.0.14, 4:=phpbb3.2.0, C:=Cancel"
+IF ERRORLEVEL 5 GOTO StepTwo
+IF ERRORLEVEL 4 GOTO LabelFour
 IF ERRORLEVEL 3 GOTO LabelThree
 IF ERRORLEVEL 2 GOTO LabelTwo
 IF ERRORLEVEL 1 GOTO LabelOne
 
 GOTO End
 
-:LabelThree
-SET src_file=%upgrade_path%\phpbb3.2
+:LabelOne
+SET src_file=%upgrade_path%\phpbb\phpbb3.0.5
 goto DoCopy
 
 :LabelTwo
-SET src_file=%upgrade_path%\phpbb3.0.14
+SET src_file=%upgrade_path%\phpbb\phpbb3.0.12
 goto DoCopy
 
-:LabelOne
-SET src_file=%upgrade_path%\live_fixed
+
+:LabelThree
+SET src_file=%upgrade_path%\phpbb\phpbb3.0.14
 goto DoCopy
+
+:LabelFour
+SET src_file=%upgrade_path%\phpbb\phpbb3.2.0
+goto DoCopy
+
+
 
 
 :DoCopy
 echo Cleaning "%forum_path%"
 
-ATTRIB +H %forum_path%\.git
+if exist %forum_path%\.git ATTRIB +H %forum_path%\.git
+
 FOR /D %%i IN (%forum_path%\*) DO rmdir /S /Q %%i
  DEL /Q %forum_path%\*.*
-ATTRIB -H %forum_path%\.git
 
 echo Copying from %src_file%
 xcopy /S %src_file% %forum_path% /E /Q
 
 :StepTwo
-CHOICE /M "Database restore?"
-IF ERRORLEVEL 2 GOTO End
-IF ERRORLEVEL 1 GOTO RestoreDatabase
+CHOICE /C 123sC /M "Database?\n 1: 3.0.5, 2: 3.0.14, 3: 3.2.0, s: 3.0.14s, C: Cancel"
+IF ERRORLEVEL 5 GOTO End
+IF ERRORLEVEL 4 GOTO RestoreDatabaseS
+IF ERRORLEVEL 3 GOTO RestoreDatabase3
+IF ERRORLEVEL 2 GOTO RestoreDatabase2
+IF ERRORLEVEL 1 GOTO RestoreDatabase1
 
 GOTO End
 
-:RestoreDatabase
 
-SET forum_db_name=db1164312_dremteam
-SET forum_db_path=G:\Personal\dreamteam\database\%forum_db_name%_clean.sql
-SET PATH=%PATH%;C:\wamp\bin\mysql\mysql5.6.17\bin;
 
-echo Cleaning MySQL for schema %forum_db_name%
+:RestoreDatabase1
+SET forum_db_path=G:\Personal\dreamteam\database\%forum_db_name%_3.0.5.sql
+goto ImportDB
+
+:RestoreDatabase2
+SET forum_db_path=G:\Personal\dreamteam\database\%forum_db_name%_3.0.14.sql
+goto ImportDB
+
+:RestoreDatabase3
+SET forum_db_path=G:\Personal\dreamteam\database\%forum_db_name%_3.2.0.sql
+goto ImportDB
+
+:RestoreDatabaseS
+SET forum_db_path=G:\Personal\dreamteam\database\%forum_db_name%_3.0.14s.sql
+goto ImportDB
+
+:ImportDB
+if not exist %forum_db_path% (
+  ECHO Database sql  file %forum_db_path% doesnot exist! 
+  goto End
+)
+echo Cleaning MySQL for schema %forum_db_name% using %forum_db_path%
+rem mysql -u root -p --execute="drop database %forum_db_name%"
 mysql -uroot -p %forum_db_name% < %forum_db_path%
 
 
